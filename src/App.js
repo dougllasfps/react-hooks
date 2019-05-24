0663b02fdf48ac29c8b4636e27f19551383744f6
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2019-05-24 08:30:13 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './custom.css'
 
-function App() {
+export default () => {
+ 
+  const [nome, setNome] = useState('') 
+  const [idade, setIdade] = useState('') 
+  const [mostrarDados, setMostrarDados] = useState(false)
+
+  function isIdadeValid(){
+     return !isNaN(idade)
+  }
+
+  function isNomeValid(){
+    if(!nome){
+      return true;
+    }
+    return nome.length > 2;
+  }
+
+ function isFormValid(){
+   return isIdadeValid() && isNomeValid()
+ }
+
+  function onSubmit(e){
+    e.preventDefault();
+    setMostrarDados(true)
+  }
+
+  function msgErro(render, message){
+    return render && <span className="msg-erro">{message}</span>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <form onSubmit={e => onSubmit(e)}>
+      <input type="text" 
+             placeholder="nome" 
+             onChange={e => setNome(e.target.value)} 
+             value={nome} />
+      { msgErro(!isNomeValid() , 'Nome inválido')}    
+      <br /><br />
+      <input type="text" 
+             placeholder="idade"
+             onChange={e => setIdade(e.target.value)} 
+             value={idade} /> 
+      { msgErro(!isIdadeValid() , 'Idade inválida') }    
+      <br /><br />
 
-export default App;
+      <button disabled={!isFormValid()} type="submit">Enviar</button> 
+      <br /><br />
+      { mostrarDados &&  <span>{`Nome: ${nome}, Idade: ${idade}`}</span> } 
+    </form>
+  )
+}
